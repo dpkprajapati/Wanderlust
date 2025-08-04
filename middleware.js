dd741cb2818a6aa2c328d07089fd5527e3709b7f
dpkprajapati <deepakk45755@gmail.com>
2025-08-04 17:02:33 +0530

    saves the requested path and used after login

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req, res, next)=>{
     if(!req.isAuthenticated()){
-        req.flash("error", "login first ")
+        req.session.redirectUrl=req.originalUrl;
+        console.log(req.session.redirectUrl)
+        req.flash("error", "Please login first ")
         return res.redirect("/login")
     }
     next();
+}
+
+module.exports.saveRedirectUrl = (req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl
+    }
+    next()
 }
