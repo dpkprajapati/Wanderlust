2b68b46ac6ff2065a4b3a7d4225719d49ae7b7a7
dpkprajapati <deepakk45755@gmail.com>
2025-08-07 22:17:23 +0530

    added owner

--- init/index.js
+++ init/index.js
@@ -15,6 +15,10 @@ main().then((res)=>{
 
 const initDb= async ()=>{
     await Listing.deleteMany({})
+   initData.data = initData.data.map((obj)=>(
+        {...obj,
+            owner:"68906e6a854609aa1bf95f7a"
+        }))
     await Listing.insertMany(initData.data);
     console.log("data was initialized")
 }
